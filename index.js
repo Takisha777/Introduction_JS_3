6c2fa142a7b788f289084696f171fef87d5a10ac
Takisha777 <alexey.kovtun.od@gmail.com>
2024-05-19 14:32:49 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+if (confirm('JavaScript появился в 1995 году?')){
+    alert('Верно')
+}else {
+    alert('JavaScript появился в 1995 году')
+}
+if (confirm('Спецификация JavaScript называется ECMAScript?')){
+    alert('Верно')
+}else {
+    alert('Спецификация JavaScript называется ECMAScript')
+}
+if (confirm('JavaScript был создан за 1 месяц?')){
+    alert('Верно')
+}else {
+    alert('JavaScript был создан за 1 месяц')
+}
